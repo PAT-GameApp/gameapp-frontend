740b067958bdc9c9b8cee44eff013ec34f2d429a
Udayan <2417210@cognizant.com>
2025-12-04 18:41:50 +0530

    add: register and admin button to navbar. redirect to oauth server on login click

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
+const VITE_OAUTH_LOGIN_URL = import.meta.env.VITE_OAUTH_LOGIN_URL;
+
 const Navbar = () => {
   const navigate = useNavigate();
   return (
@@ -9,10 +11,16 @@ const Navbar = () => {
         <Link to="/" className="navbar-logo">
           <span className="logo-text">Cognizant</span>
         </Link>
-        
+
         <div className="navbar-right">
-          <button className="login-btn" onClick={() => navigate('/login')}>
+          <button className="login-btn" onClick={() => window.location.href = VITE_OAUTH_LOGIN_URL}>
             Login
+          </button>
+          <button className="register-btn" onClick={() => navigate('/register')}>
+            Register
+          </button>
+          <button className="register-btn" onClick={() => navigate('/admin')}>
+            Admin
           </button>
         </div>
       </div>
